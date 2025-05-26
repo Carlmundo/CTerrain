10ee3c811a6c4a27846d4936a108ce642038bc9c
Carl Norton <Carlmundo@users.noreply.github.com>
2025-05-26 21:15:41 +0100

    Move libraries to Data\CTerrain\

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using System.Runtime.Loader;
+
 namespace W2_Terrain_Loader
 {
     internal static class Program
@@ -15,6 +18,16 @@ namespace W2_Terrain_Loader
             if (!createdNew) {
                 return;
             }
+
+            AssemblyLoadContext.Default.Resolving += (context, assemblyName) =>
+            {
+                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data\\CTerrain", assemblyName.Name + ".dll");
+                if (File.Exists(path)) {
+                    return context.LoadFromAssemblyPath(path);
+                }
+                return null;
+            };
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
