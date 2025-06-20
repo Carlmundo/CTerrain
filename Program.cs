f0a84f914afd76104c47d5860d90fe2b08fb69f3
Carl Norton <Carlmundo@users.noreply.github.com>
2025-06-20 12:31:33 +0100

    Revert "Move libraries to Data\CTerrain\"
    This reverts commit 10ee3c811a6c4a27846d4936a108ce642038bc9c.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-using System.Reflection;
-using System.Runtime.Loader;
-
 namespace W2_Terrain_Loader
 {
     internal static class Program
@@ -18,16 +15,6 @@ namespace W2_Terrain_Loader
             if (!createdNew) {
                 return;
             }
-
-            AssemblyLoadContext.Default.Resolving += (context, assemblyName) =>
-            {
-                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data\\CTerrain", assemblyName.Name + ".dll");
-                if (File.Exists(path)) {
-                    return context.LoadFromAssemblyPath(path);
-                }
-                return null;
-            };
-
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
